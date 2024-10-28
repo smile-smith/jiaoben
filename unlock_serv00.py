447c8301439a670536aad9b226b855cfed58527a
smile-smith <160609353+smile-smith@users.noreply.github.com>
2024-10-28 14:45:32 +0800

    增加了server的注释

--- unlock_serv00.py
+++ unlock_serv00.py
@@ -5,8 +5,25 @@ url = 'https://www.serv00.com/ip_unban/'
 
 # 表单数据，根据你的HTML，我们假设需要提交server字段
 data = {
-    'server': '7'  # 选择一个服务器，例如's9.serv00.com'
+    'server': '7'  # 选择一个服务器
 }
+# server列表
+""" 
+server: '16'-------  s13.serv00.com
+server: '15'-------  s12.serv00.com
+server: '14'-------  s11.serv00.com
+server: '13'-------  s10.serv00.com
+server: '12'-------  s9.serv00.com
+server: '11'-------  s8.serv00.com
+server: '10'-------  s7.serv00.com
+server: '9' -------  s6.serv00.com
+server: '8' -------  s5.serv00.com
+server: '7' -------  s4.serv00.com
+server: '6' -------  s3.serv00.com
+server: '5' -------  s2.serv00.com
+server: '4' -------  s1.serv00.com
+server: '1' -------  s0.serv00.com
+"""
 
 # 请求头，可能需要包含CSRF令牌
 headers = {
